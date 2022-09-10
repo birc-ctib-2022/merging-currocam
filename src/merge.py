a004aa26d846d4d7e41f922c1853c3a5666e9f5b
currocam <campuzanocurro@gmail.com>
2022-09-10 14:23:01 +0200

    feat: merge

--- src/merge.py
+++ src/merge.py
@@ -16,7 +16,10 @@ def merge(x: list[int], y: list[int]) -> list[int]:
     # FIXME: fill out the loop so you merge the lists
     # until one of them is empty
     while i < len(x) and j < len(y):
-        break  # FIXME: you shouldn't just break here
-    # At least one of the lists is empty now. Copy the
-    # remainder of the other into z.
-    return z
+        if x[i] < y[j]:
+            z.append(x[i])
+            i += 1
+        else:
+            z.append(y[j])
+            j += 1
+    return z + x[i:len(x)] + y[j:len(y)]
